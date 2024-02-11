db33f5a6857c385d387910e94babdd3e92b5f05b
aritabryan <147879047+aritabryan@users.noreply.github.com>
2024-02-11 14:06:50 -0600

    Update Tarea2_Progra1.java

--- Tarea#2_Progra1/src/tarea/pkg2_progra1/Tarea2_Progra1.java
+++ Tarea#2_Progra1/src/tarea/pkg2_progra1/Tarea2_Progra1.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package tarea.pkg2_progra1;
 
+package tarea.pkg2_progra1;
+//Gabriel Mejia y Bryan Arita
 import java.util.Scanner;
 
 /**
@@ -18,7 +15,7 @@ public class Tarea2_Progra1 {
     static Scanner duki = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("***Menu***\n1.Replace\n 2.Factorial\n3. Mensajes\n4. Promedios\n5. Aprobado o reprobado\n6. Contains\n");
+        System.out.println("***Menu***\n1.Replace\n2.Factorial\n3. Mensajes\n4. Promedios\n5. Aprobado o reprobado\n6. Contains\n");
         System.out.println("Elije una opcion: ");
         int opc = duki.nextInt();
         char resp = 's';
@@ -26,14 +23,28 @@ public class Tarea2_Progra1 {
         while (resp == 's' || resp == 'S') {
             switch (opc) {
                 case 1:
+                    System.out.print("Ingrese su cadena: ");
+                    String cadena = duki.next();
+                    System.out.print("Ingrese el caracter que desea remplazar: ");
+                    char remp2 = duki.next().charAt(0);
+                    System.out.print("Ingrese su nuevo caracter a remplazar: ");
+                    char nuevo = duki.next().charAt(0);
+
+                    String imprimirRep = Replace(cadena, nuevo, remp2);
+                    System.out.println(imprimirRep);
                     break;
                 case 2:
+                   System.out.print("Ingrese el numero que desea saber el factorial: ");
+                    int num = duki.nextInt();
+                    String fact = factorial(num);
+                    System.out.println(fact);
                     break;
                 case 3:
                     System.out.println("Ingrese una cadena:");
                     mensaje(duki.nextLine());
                     break;
                 case 4:
+                    promedio();
                     break;
                 case 5:
                     System.out.println("Ingrese la nota del alumno: ");
@@ -63,7 +74,7 @@ public class Tarea2_Progra1 {
             resp = duki.next().charAt(0);
             if (resp == 's' || resp == 'S') {
 
-                System.out.println("***Menu***\n1. Replace\n 2. Factorial\n3. Mensajes\n4. Promedios\n5. Aprobado o reprobado\n6. Contains\n");
+                System.out.println("***Menu***\n1. Replace\n2. Factorial\n3. Mensajes\n4. Promedios\n5. Aprobado o reprobado\n6. Contains\n");
                 System.out.println("Selecione una opcion: ");
                 opc = duki.nextInt();
             }
@@ -93,4 +104,49 @@ public class Tarea2_Progra1 {
         }
         return false;
     }
+    
+     public static void promedio() {
+        char resp = 's';
+        while (resp == 's' || resp == 'S') {
+            System.out.print("Ingrese nota 1: ");
+            int nota1 = duki.nextInt();
+            System.out.print("Ingrese nota 2: ");
+            int nota2 = duki.nextInt();
+            System.out.print("Ingrese nota 3: ");
+            int nota3 = duki.nextInt();
+            System.out.print("Ingrese nota 4: ");
+            int nota4 = duki.nextInt();
+            int promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+            System.out.println("Promedio de las notas: " + promedio);
+            System.out.print("Desea continuar?[S/N]: ");
+            resp = duki.next().charAt(0);
+        }
+    }
+
+    public static String factorial(int n) {
+        boolean coma = true;
+        String acumfactorial = "";
+        int cont = 1;
+        for (int i = 1; i < n + 1; i++) {
+            cont *= i;
+            if (!coma) {
+                acumfactorial += "x";
+            }
+            coma = false;
+            acumfactorial += i;
+            if (i == n) {
+                acumfactorial += "=";
+                acumfactorial += cont;
+            }
+        }
+        return acumfactorial;
+    }
+
+    public static String Replace(String cadena, char nuevo, char viejo) {
+        String rep = "";
+        String nuevacadena=cadena.replace(viejo, nuevo);
+        rep += nuevacadena;
+        return rep;
+    }
+    
 }
